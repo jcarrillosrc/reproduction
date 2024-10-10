7785b4c029b720ad802f2302ca159859f1515820
Jaime Carrillo <jaime.a.web@gmail.com>
2024-10-10 09:08:55 +0200

    Entities are updated after find them or populate them from parent entity

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,51 +1,217 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import {
+  Collection,
+  DecimalType,
+  defineConfig,
+  Embedded,
+  Entity,
+  ManyToOne,
+  MikroORM,
+  OneToMany,
+  PrimaryKey,
+  Property,
+} from "@mikro-orm/postgresql";
+import { Money } from "./money.embeddable";
 
-@Entity()
+@Entity({ tableName: "test_user" })
 class User {
-
   @PrimaryKey()
   id!: number;
 
   @Property()
   name: string;
 
-  @Property({ unique: true })
+  @Property()
   email: string;
 
-  constructor(name: string, email: string) {
+  @Property({ type: "decimal", scale: 2 })
+  decimal: number | null = null;
+
+  @OneToMany<Book, User>({
+    entity: () => Book,
+    mappedBy: (book) => book.user,
+    orderBy: {
+      id: "ASC",
+    },
+  })
+  books = new Collection<Book, this>(this);
+
+  constructor(
+    id: number,
+    name: string,
+    email: string,
+    decimal: number | null = null
+  ) {
+    this.id = id;
     this.name = name;
     this.email = email;
+    this.decimal = decimal;
   }
-
 }
 
-let orm: MikroORM;
+@Entity({ tableName: "test_books" })
+class Book {
+  @PrimaryKey()
+  id!: number;
 
-beforeAll(async () => {
-  orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    debug: ['query', 'query-params'],
-    allowGlobalContext: true, // only for testing
+  @Property()
+  name: string;
+
+  @Property({
+    type: new DecimalType("number"),
+    runtimeType: "number",
+    scale: 2,
+  })
+  decimalAmount: number;
+
+  @Embedded(() => Money)
+  price: Money;
+
+  @ManyToOne<Book, User>({
+    entity: () => User,
+    inversedBy: (user) => user.books,
+  })
+  user: User;
+
+  constructor(
+    id: number,
+    name: string,
+    decimalAmount: number,
+    price: Money,
+    user: User
+  ) {
+    this.id = id;
+    this.name = name;
+    this.decimalAmount = decimalAmount;
+    this.price = price;
+    this.user = user;
+  }
+}
+
+describe("should not update entities just for find them inside a transaction", () => {
+  let orm: MikroORM;
+  let loggerMessages: string[] = [];
+
+  beforeAll(async () => {
+    orm = await MikroORM.init(
+      defineConfig({
+        clientUrl: "postgresql://admin:admin@localhost:5432/admin_api",
+        entities: [User, Book],
+        debug: ["query", "query-params"],
+        allowGlobalContext: true, // only for testing
+        forceEntityConstructor: true,
+        logger: (message: string) => {
+          loggerMessages.push(message);
+        },
+      })
+    );
+    await orm.schema.refreshDatabase();
   });
-  await orm.schema.refreshDatabase();
-});
 
-afterAll(async () => {
-  await orm.close(true);
-});
+  beforeEach(() => {
+    loggerMessages = [];
+  });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
+  afterAll(async () => {
+    await orm.close(true);
+  });
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  test("should not update book entity when findOne a user WITHOUT books", async () => {
+    await orm.em.transactional(async () => {
+      const user = orm.em.create(User, {
+        id: 1,
+        name: "Foo",
+        email: "foo",
+        decimal: 2.22,
+      });
+      user.books.add(
+        new Book(1, "book-1", 11.45, new Money(10.54, "USD"), user)
+      );
+    });
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+    orm.em.clear();
+
+    await orm.em.transactional(async () => {
+      await orm.em.findOne(User, {
+        id: 1,
+      });
+    });
+
+    const unwantedUpdate = loggerMessages.find((message: string) => {
+      return /(update|(update \"test_books\" set \"decimal_amount\" = 11.45, \"price_amount\" = 10.54 where \"id\" = 1))/gi.test(
+        message
+      );
+    });
+
+    expect(unwantedUpdate).toBeFalsy();
+  });
+
+  test("should not update book entity when findOne a user WITH books", async () => {
+    await orm.em.transactional(async () => {
+      const user = orm.em.create(User, {
+        id: 2,
+        name: "Foo",
+        email: "foo",
+        decimal: 2.22,
+      });
+      user.books.add(
+        new Book(2, "book-2", 11.45, new Money(10.54, "USD"), user)
+      );
+    });
+
+    orm.em.clear();
+
+    await orm.em.transactional(async () => {
+      await orm.em.findOne(
+        User,
+        {
+          id: 2,
+        },
+        {
+          populate: ["books"],
+        }
+      );
+    });
+
+    const unwantedUpdate = loggerMessages.find((message: string) => {
+      return /(update|(update \"test_books\" set \"decimal_amount\" = 11.45, \"price_amount\" = 10.54 where \"id\" = 1))/gi.test(
+        message
+      );
+    });
+
+    expect(unwantedUpdate).toBeFalsy();
+  });
+
+  test("should not update book entities when find them after creating using constructors", async () => {
+    await orm.em.transactional(async (em) => {
+      const user = new User(3, "Foo", "foo", 2.22);
+      const book = new Book(3, "book-3", 11.45, new Money(10.54, "USD"), user);
+
+      user.books.add(book);
+
+      em.persist(user);
+      em.persist(book);
+    });
+
+    orm.em.clear();
+
+    await orm.em.transactional(async () => {
+      await orm.em.findOne(
+        User,
+        {
+          id: 3,
+        },
+        {
+          populate: ["books"],
+        }
+      );
+    });
+
+    const unwantedUpdate = loggerMessages.find((message: string) => {
+      return /(update|(update \"test_books\" set \"decimal_amount\" = 11.45, \"price_amount\" = 10.54 where \"id\" = 1))/gi.test(
+        message
+      );
+    });
+
+    expect(unwantedUpdate).toBeFalsy();
+  });
 });
---
+++ src/money.embeddable.ts
@@ -0,0 +1,19 @@
+import { DecimalType, Embeddable, Property } from "@mikro-orm/core";
+
+@Embeddable()
+export class Money {
+  @Property({
+    type: new DecimalType('number'),
+    runtimeType: 'number',
+    scale: 2,
+  })
+  amount: number;
+
+  @Property({ length: 3 })
+  currencyCode: string;
+
+  constructor(amount: number, currencyCode: string) {
+    this.amount = amount;
+    this.currencyCode = currencyCode;
+  }
+}
